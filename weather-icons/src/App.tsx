7cb4d8e3214b8457c75c6cebad8240e6f635326e
milky <listingslab@gmail.com>
2023-04-29 15:37:01 +0200

    weather-icons setup
    Co-Authored-By: 魏臧 <weizang75@gmail.com>

--- weather-icons/src/App.tsx
+++ weather-icons/src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import {
+  Avatar,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  CardHeader,
+  Typography,
+  CardMedia,
+} from "@mui/material"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Dialog 
+        open 
+        fullScreen
+        onClose={(e: React.MouseEvent) => {
+          e.preventDefault()
+          // dispatch(setOpen(false))
+        }}
+        fullWidth
+        maxWidth="md"
+        PaperProps={{
+          sx: {
+            height: "100vh"
+          }
+        }}
+
+      >
+        <DialogTitle>
+          <CardHeader 
+            avatar={<Avatar src="/logo.svg" />}
+            title={<Typography variant="h6">
+                      Weather Icons
+                    </Typography>}
+            subheader={<Typography variant="body1">
+            In this example we use create react app to build some 
+            Weather Icon Components we need for another project
+            </Typography>}
+          />
+          
+        </DialogTitle>
+        
+        <DialogContent>
+          
+        <CardMedia 
+          component={"img"}
+          height={125}
+          src="/svg/All_Wether_Icons.svg"
+        />
+        </DialogContent>
+        <DialogActions>
+          <Button variant="contained">
+            Next
+          </Button>
+        </DialogActions>
+    </Dialog>
+  )
 }
 
-export default App;
+export default App
